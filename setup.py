5fdc1e45ee069bb03c56c6b50c635c87b1f68ec0
MichaelYin <hgyinjian@gmail.com>
2018-11-30 23:36:31 +0800

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='wagtail-whoosh',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     license='BSD License',
