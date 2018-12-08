866276937f2e537f6decb8c849423fb58cf1022b
MichaelYin <hgyinjian@gmail.com>
2018-12-08 10:39:49 +0800

    clean code

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,13 +1,8 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
-from django.core.management import call_command
 from django.test import TestCase
-from django.utils.six import StringIO
-# from wagtail.tests.search.models import SearchTest
 from wagtail.search.tests.test_backends import BackendTests
-from wagtail.tests.search import models
-from wagtail.search.query import MATCH_ALL, And, Boost, Filter, Not, Or, PlainText, Term
 
 
 class TestWhooshSearchBackend(BackendTests, TestCase):
