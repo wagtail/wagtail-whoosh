589e8fab859110cf9303bb0c99c28c120dc22cd6
Michaelyin <hgyinjian@gmail.com>
2020-09-14 20:32:22 +0800

    format file

--- tests/project/management/commands/benchmark.py
+++ tests/project/management/commands/benchmark.py
@@ -12,22 +12,23 @@ from wagtail.contrib.postgres_search.models import IndexEntry
 from wagtail.search.backends import get_search_backend
 from wagtail.search.backends.db import DatabaseSearchBackend
 from wagtail.search.signal_handlers import (
-    post_save_signal_handler, post_delete_signal_handler)
+    post_save_signal_handler,
+    post_delete_signal_handler,
+)
 
 from ...models import Article
 
 
 def format_time(s):
-    for unit in ('s', 'ms', 'μs'):
+    for unit in ("s", "ms", "μs"):
         if s > 1:
-            return '%.2f %s' % (s, unit)
+            return "%.2f %s" % (s, unit)
         s *= 1000
-    return '%.2f ns' % s
+    return "%.2f ns" % s
 
 
 def strictly_positive_int(value):
-    type_error = ArgumentTypeError('%s must be a strictly positive integer.'
-                                   % value)
+    type_error = ArgumentTypeError("%s must be a strictly positive integer." % value)
     try:
         value = int(value)
     except (TypeError, ValueError):
@@ -37,7 +38,7 @@ def strictly_positive_int(value):
     return value
 
 
-def time_it(func, title='', backend_name=''):
+def time_it(func, title="", backend_name=""):
     n = 3
     start = time()
     while True:
@@ -47,30 +48,40 @@ def time_it(func, title='', backend_name=''):
         if elapsed_time > 1:
             break
         n *= 10
-    print('%-30s %-15s %9s %15s'
-          % (title, backend_name,
-             format_time(elapsed_time / n), '%s runs' % n))
+    print(
+        "%-30s %-15s %9s %15s"
+        % (title, backend_name, format_time(elapsed_time / n), "%s runs" % n)
+    )
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('-d', '--dictionary', type=FileType('r'),
-                            default='/usr/share/dict/words',
-                            help='Path to a dictionary file '
-                                 'containing a word per line.')
-        parser.add_argument('-s', '--start',
-                            type=strictly_positive_int, default=1)
-        parser.add_argument('-m', '--step-magnitude',
-                            type=strictly_positive_int, default=5)
-        parser.add_argument('--seed', default='random seed')
-        parser.add_argument('-l', '--limit', type=strictly_positive_int,
-                            default=1000000)
+        parser.add_argument(
+            "-d",
+            "--dictionary",
+            type=FileType("r"),
+            default="/usr/share/dict/words",
+            help="Path to a dictionary file " "containing a word per line.",
+        )
+        parser.add_argument("-s", "--start", type=strictly_positive_int, default=1)
+        parser.add_argument(
+            "-m", "--step-magnitude", type=strictly_positive_int, default=5
+        )
+        parser.add_argument("--seed", default="random seed")
+        parser.add_argument(
+            "-l", "--limit", type=strictly_positive_int, default=1000000
+        )
 
     def create_article_bulk(self, dictionary, bulk_size):
-        Article.objects.bulk_create([Article(
-                title=' '.join(choices(dictionary, k=randint(3, 10))),
-                body=' '.join(choices(dictionary, k=randint(50, 300))))
-            for i in range(bulk_size)])
+        Article.objects.bulk_create(
+            [
+                Article(
+                    title=" ".join(choices(dictionary, k=randint(3, 10))),
+                    body=" ".join(choices(dictionary, k=randint(50, 300))),
+                )
+                for i in range(bulk_size)
+            ]
+        )
 
     @property
     def backends(self):
@@ -78,16 +89,21 @@ class Command(BaseCommand):
             yield backend_name, get_search_backend(backend=backend_name)
 
     def update_index(self, backend_name):
-        call_command('update_index', backend_name=backend_name,
-                     stdout=StringIO())
+        call_command("update_index", backend_name=backend_name, stdout=StringIO())
 
     def unsorted_autocomplete(self, backend):
-        list(backend.autocomplete(self.partial_word, Article,
-                                  order_by_relevance=False)[:10])
+        list(
+            backend.autocomplete(self.partial_word, Article, order_by_relevance=False)[
+                :10
+            ]
+        )
 
     def sorted_autocomplete(self, backend):
-        list(backend.autocomplete(self.partial_word, Article,
-                                  order_by_relevance=True)[:10])
+        list(
+            backend.autocomplete(self.partial_word, Article, order_by_relevance=True)[
+                :10
+            ]
+        )
 
     def unsorted_search(self, backend):
         list(backend.search(self.word, Article, order_by_relevance=False)[:10])
@@ -96,13 +112,16 @@ class Command(BaseCommand):
         list(backend.search(self.word, Article, order_by_relevance=True)[:10])
 
     def step(self, dictionary, bulk_size):
-        print('Adding %s articles…' % bulk_size, end='\r')
+        print("Adding %s articles…" % bulk_size, end="\r")
         self.create_article_bulk(dictionary, bulk_size)
-        print('Testing with %s articles:' % Article.objects.count())
+        print("Testing with %s articles:" % Article.objects.count())
         for backend_name, backend in self.backends:
-            time_it(lambda: self.update_index(backend_name),
-                    title='Update index', backend_name=backend_name)
-        print('Optimizing the database…', end='\r')
+            time_it(
+                lambda: self.update_index(backend_name),
+                title="Update index",
+                backend_name=backend_name,
+            )
+        print("Optimizing the database…", end="\r")
         with connection.cursor() as cursor:
             for model in (Article, IndexEntry):
                 table = model._meta.db_table
@@ -110,15 +129,26 @@ class Command(BaseCommand):
                 cursor.execute('REINDEX TABLE "%s";' % table)
         for backend_name, backend in self.backends:
             if not isinstance(backend, DatabaseSearchBackend):
-                time_it(lambda: self.unsorted_autocomplete(backend),
-                        title='Unsorted autocomplete',
-                        backend_name=backend_name)
-                time_it(lambda: self.sorted_autocomplete(backend),
-                        title='Sorted autocomplete', backend_name=backend_name)
-            time_it(lambda: self.unsorted_search(backend),
-                    title='Unsorted search', backend_name=backend_name)
-            time_it(lambda: self.sorted_search(backend),
-                    title='Sorted search', backend_name=backend_name)
+                time_it(
+                    lambda: self.unsorted_autocomplete(backend),
+                    title="Unsorted autocomplete",
+                    backend_name=backend_name,
+                )
+                time_it(
+                    lambda: self.sorted_autocomplete(backend),
+                    title="Sorted autocomplete",
+                    backend_name=backend_name,
+                )
+            time_it(
+                lambda: self.unsorted_search(backend),
+                title="Unsorted search",
+                backend_name=backend_name,
+            )
+            time_it(
+                lambda: self.sorted_search(backend),
+                title="Sorted search",
+                backend_name=backend_name,
+            )
         print()
 
     def clear(self):
@@ -139,16 +169,16 @@ class Command(BaseCommand):
             post_delete.connect(post_delete_signal_handler, sender=Article)
 
     def handle(self, *args, **options):
-        dictionary = list(options['dictionary'])
+        dictionary = list(options["dictionary"])
         while True:
             self.word = choice(dictionary)
             if len(self.word) >= 6:
                 break
         self.partial_word = self.word[:3]
-        start = options['start']
-        step_magnitude = options['step_magnitude']
-        limit = options['limit']
-        seed(options['seed'])
+        start = options["start"]
+        step_magnitude = options["step_magnitude"]
+        limit = options["limit"]
+        seed(options["seed"])
         with self.set_up():
             n = start
             while n <= limit:
